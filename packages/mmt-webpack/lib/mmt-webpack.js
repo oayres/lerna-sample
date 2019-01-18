85b63cab61b9a8d742902d73c9ea1cd5d97fa2bd
Mark White <mark.w@mmtdigital.co.uk>
2019-01-18 16:57:02 +0000

    feat(pencil): add 'graphiteWidth' option

--- packages/mmt-webpack/lib/mmt-webpack.js
+++ packages/mmt-webpack/lib/mmt-webpack.js
@@ -46,6 +46,8 @@ const mergeConfigs = (rootPath, mmtConfig, mmtWebpackConfigExtensions, env, argv
   if (mmtConfig.verbose || isProd) {
     console.log('Building with the following webpack config', JSON.stringify(mergedConfig, null, 2))
   }
+
+
   return mergedConfig
 }
 
